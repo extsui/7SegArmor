7c7f31e7202bbe58ba022818ef0926dbe7fc3068
extsui <en422641@s.okayama-u.ac.jp>
2018-03-19 00:28:26 +0900

    デバッグ/リブート/テストコマンドの追加
    コマンド詳細はコマンドテーブル参照。
    同時に複数パラメータ解析関数を追加。

--- src/command.c
+++ src/command.c
@@ -7,10 +7,13 @@
 #include "finger.h"
 #include <stdio.h>
 #include <string.h> /* memset() */
+#include <stdlib.h> /* strtol() */
 
 /************************************************************
  *  define
  ************************************************************/
+// テスト実施時に有効化すること。
+//#define	TEST_COMMAND
 
 #define COMMAND_BUF_SIZE	(256)
 
@@ -64,18 +67,24 @@ static void cmdHelp(const char *s);
 static void cmdVersion(const char *s);
 static void cmdSet(const char *s);
 static void cmdGet(const char *s);
+static void cmdDebug(const char *s);
+static void cmdReboot(const char *s);
+static void cmdTest(const char *s);
 
 static const Command g_CommandTable[] = {
-	{ "C0",	2,	cmdC0,		"Get Info",					},
-	{ "C1",	2,	cmdC1,		"Set Display",				},
-	{ "C2",	2,	cmdC2,		"Set Brightness",			},
-	{ "C3",	2,	cmdC3,		"Set Chain Display",		},
-	{ "C4", 2,	cmdC4,		"Set Chain Brightness",		},
-	{ "C5",	2,	cmdC5,		"Update All Chain",			},
-	{ "-h",	2,	cmdHelp,	"Help",						},
-	{ "-v",	2,	cmdVersion,	"Version",					},
-	{ "s",	1,	cmdSet,		"Set Param : s <p1> <p2>",	},
-	{ "g",	1,	cmdGet,		"Get Param : g <p1>",		},
+	{ "C0",		2,	cmdC0,		"Get Info",					},
+	{ "C1",		2,	cmdC1,		"Set Display",				},
+	{ "C2",		2,	cmdC2,		"Set Brightness",			},
+	{ "C3",		2,	cmdC3,		"Set Chain Display",		},
+	{ "C4", 	2,	cmdC4,		"Set Chain Brightness",		},
+	{ "C5",		2,	cmdC5,		"Update All Chain",			},
+	{ "-h",		2,	cmdHelp,	"Help",						},
+	{ "-v",		2,	cmdVersion,	"Version",					},
+	{ "s",		1,	cmdSet,		"Set Param : s <p1> <p2>",	},
+	{ "g",		1,	cmdGet,		"Get Param : g <p1>",		},
+	{ "dbg",	3,	cmdDebug,	"Debug : dbg <p1> <p2>",	},
+	{ "reb",	3,	cmdReboot,	"Reboot"					},
+	{ "test",	4,	cmdTest,	"Test"						},
 };
 
 // TODO: armor.cに移動するべき。
@@ -128,6 +137,24 @@ static void armorFrameSendendProc(void);
  * エラー処理は行わない。
  */
 static uint8_t atoh(uint8_t c_h, uint8_t c_l);
+
+/**
+ * 複数パラメータ解析
+ * 
+ * 文字列sからnum個のパラメータを解析してparams[]に格納する。
+ * 格納出来た個数を戻り値として返す。
+ * パラメータは空白文字(" ")で区切られているものとする。
+ * num個分の解析が終了するか、もしくは'\0'が来たら終了する。
+ *
+ * @param s 入力文字列(NULLでないこと)
+ * @param params 解析結果配列(NULLでないこと)
+ * @param num 解析する個数(0より大)
+ * @return 解析できた個数
+ * @note 解析可能な値はstrtol()で基数0指定時のものに限る。
+ * @note また、解析結果はint型に丸められることに注意。
+ * @note 引数の前提に記載した項目はチェックしない。
+ */
+static int getParam(const char *s, int params[], int num);
 
 /************************************************************
  *  public functions
@@ -203,6 +230,8 @@ static void Command_onReceive(const char *rxBuff)
 	}
 }
 
+/************************************************************/
+
 static void cmdC0(const char *s)
 {
 	uint8_t dipsw;
@@ -262,24 +291,7 @@ static void cmdC2(const char *s)
 
 static void cmdC3(const char *s)
 {
-	// 連結表示設定コマンド
-	// TODO:
-	// 最終的にはシリアルから設定したデータを送るようにする。
-	// 開発中は固定データを送ってデバッグする。
-
-	// <通信プロトコル>
-	// [0    ]: 固定で1 (表示コマンドID)
-	// [1..32]: 7セグ表示データ(左上から右下に向かう順に指定)
-	Finger_setDisplayAll(&test_data[1]);
-			
-	// TODO: フラグを直接操作してはいけない。関数でラッピングするべき。
-	// そもそも1フレームしか用意しないじゃなかったっけ？
-	g_IsArmorFrameSending = TRUE;
-	g_ArmorFrameSendNum = 4;
-	g_ArmorFrameSendCount = 1;
 	
-	// 33バイトのDMA送信@1MHz≒270[us]
-	R_DMAC1_StartSend((uint8_t *)&test_data[g_ArmorFrameSendCount*33], 33);
 }
 
 static void cmdC4(const char *s)
@@ -309,12 +321,64 @@ static void cmdVersion(const char *s)
 
 static void cmdSet(const char *s)
 {
-	DPRINTF("Not Implemented.\n");
+	int i;
+	int pnum = 0;
+	int params[2];
+	
+	pnum = getParam(s, params, 2);
+	
+	DPRINTF("pnum = %d\n", pnum);
+	for (i = 0; i < pnum; i++) {
+		DPRINTF("params[%d] = %d\n", i, params[i]);
+	}
+	
 }
 
 static void cmdGet(const char *s)
 {
+	
+	
 	DPRINTF("Not Implemented.\n");
+}
+
+static void cmdDebug(const char *s)
+{
+	// 連結表示設定コマンド
+	// TODO:
+	// 最終的にはシリアルから設定したデータを送るようにする。
+	// 開発中は固定データを送ってデバッグする。
+
+	// <通信プロトコル>
+	// [0    ]: 固定で1 (表示コマンドID)
+	// [1..32]: 7セグ表示データ(左上から右下に向かう順に指定)
+	Finger_setDisplayAll(&test_data[1]);
+			
+	// TODO: フラグを直接操作してはいけない。関数でラッピングするべき。
+	// そもそも1フレームしか用意しないじゃなかったっけ？
+	g_IsArmorFrameSending = TRUE;
+	g_ArmorFrameSendNum = 4;
+	g_ArmorFrameSendCount = 1;
+	
+	// 33バイトのDMA送信@1MHz≒270[us]
+	R_DMAC1_StartSend((uint8_t *)&test_data[g_ArmorFrameSendCount*33], 33);
+}
+
+static void cmdReboot(const char *s)
+{
+	DPRINTF("Reboot after about 1sec. (by WDT)\n");
+	while (1);
+}
+
+static void cmdTest(const char *s)
+{
+#ifdef TEST_COMMAND
+	// プロトタイプ宣言を外に出すとテスト追加時の
+	// 修正箇所が飛んでしまうのでローカルで宣言する。
+	void test_getParam(void);
+	test_getParam();
+#else
+	DPRINTF("You should activate \"TEST_COMMAND\".\n");
+#endif
 }
 
 /************************************************************/
@@ -358,6 +422,9 @@ void Command_masterSendendHandler(void)
 	}
 }
 
+/************************************************************
+ *  prvaite functions
+ ************************************************************/
 static void armorFrameSendendProc(void)
 {
 	// 最速だとINTC割り込みを取りこぼすので遅延が必要。
@@ -368,9 +435,6 @@ static void armorFrameSendendProc(void)
 	g_IsArmorFrameSending = FALSE;
 }
 
-/************************************************************
- *  prvaite functions
- ************************************************************/
 static uint8_t atoh(uint8_t c_h, uint8_t c_l)
 {
 	uint8_t h, l;
@@ -401,3 +465,112 @@ static uint8_t atoh(uint8_t c_h, uint8_t c_l)
 	hex = (h << 4) | (l & 0x0F);
 	return hex;
 }
+
+static int getParam(const char *s, int params[], int num)
+{
+	int ok_num = 0;
+	char *startp = (char*)s;
+	char *endp = NULL;
+	long val = 0;
+	
+	do {
+		val = strtol(startp, &endp, 0);
+		// 1文字以上読めているなら何かしら解析できている。
+		if (startp != endp) {
+			params[ok_num] = (int)val;
+			ok_num++;
+		// 何も解析できなかったけど文字列最後でもない。
+		} else if (val == 0) {
+			break;
+		}
+		startp = endp;
+	} while ((endp != NULL) && (ok_num < num));
+	
+	return ok_num;
+}
+
+/************************************************************
+ *  テストコード
+ ************************************************************/
+#ifdef TEST_COMMAND
+
+void test_getParam(void)
+{
+	int params[2];
+	int retval;
+	
+	DPRINTF("TEST: getParam()\n");
+	
+	DPRINTF("[正常系] 引数1個、0\n");
+	retval = 0;
+	memset(params, 0, sizeof(params));
+	retval = getParam("1", params, 1);
+	ASSERT(retval == 1);
+	ASSERT(params[0] == 1);
+	
+	DPRINTF("[正常系] 引数1個、非0\n");
+	retval = 0;
+	memset(params, 0, sizeof(params));
+	retval = getParam("1", params, 1);
+	ASSERT(retval == 1);
+	ASSERT(params[0] == 1);
+
+	DPRINTF("[正常系] 引数1個、複数文字\n");
+	retval = 0;
+	memset(params, 0, sizeof(params));
+	retval = getParam("123", params, 1);
+	ASSERT(retval == 1);
+	ASSERT(params[0] == 123);
+
+	DPRINTF("[異常系] 引数1個、文字無し\n");
+	retval = 0;
+	memset(params, 0, sizeof(params));
+	params[0] = 0xCC;
+	retval = getParam("", params, 1);
+	ASSERT(retval == 0);
+	ASSERT(params[0] == 0xCC);
+
+	DPRINTF("[異常系] 引数1個、改行のみ\n");
+	retval = 0;
+	memset(params, 0, sizeof(params));
+	params[0] = 0xCC;
+	retval = getParam("\n", params, 1);
+	ASSERT(retval == 0);
+	ASSERT(params[0] == 0xCC);
+
+	DPRINTF("[異常系] 引数1個、不明文字\n");
+	retval = 0;
+	memset(params, 0, sizeof(params));
+	params[0] = 0xCC;
+	retval = getParam("$", params, 1);
+	ASSERT(retval == 0);
+	ASSERT(params[0] == 0xCC);
+	
+	DPRINTF("[正常系] 引数2個、最小文字列\n");
+	retval = 0;
+	memset(params, 0, sizeof(params));
+	retval = getParam(" 1 2", params, 2);
+	ASSERT(retval == 2);
+	ASSERT(params[0] == 1);
+	ASSERT(params[1] == 2);
+	
+	DPRINTF("[正常系] 引数2個、複数文字列\n");
+	retval = 0;
+	memset(params, 0, sizeof(params));
+	retval = getParam("123  456", params, 2);
+	ASSERT(retval == 2);
+	ASSERT(params[0] == 123);
+	ASSERT(params[1] == 456);
+	
+	DPRINTF("[異常系] 引数2個、3個指定\n");
+	retval = 0;
+	memset(params, 0, sizeof(params));
+	retval = getParam("123 456 $", params, 2);
+	ASSERT(retval == 2);
+	ASSERT(params[0] == 123);
+	ASSERT(params[1] == 456);
+	
+	DPRINTF("TEST: OK!\n");
+}
+
+#endif /* TEST_COMMAND */
